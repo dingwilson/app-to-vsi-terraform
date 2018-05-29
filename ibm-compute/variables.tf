ce684248ac28f5fd90b0cb22bfba00425eeb1cb6
Wilson Ding <hello@wilsonding.com>
2018-05-29 10:58:57 -0500

    Added ibm_sl_username

--- ibm-compute/variables.tf
+++ ibm-compute/variables.tf
@@ -25,6 +25,10 @@ variable "ibm_sl_api_key" {
 	default = "7821ca2de9a53215e62d6be12f4d509fb623fd92128839d49fb5e7caf314ba7c"	
 }
 
+variable "ibm_sl_username" {
+	default = "1416455_wilson.ding1@ibm.com"
+}
+
 # Name of the Virtual Instance based on the App Name
 variable "vi-instance-name" {
 	default = "vi-test-01"
