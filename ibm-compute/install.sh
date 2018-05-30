a1ac9d854d962110df396090d350e214683f30f2
Wilson Ding <hello@wilsonding.com>
2018-05-30 11:21:03 -0500

    Fixed install script

--- ibm-compute/install.sh
+++ ibm-compute/install.sh
@@ -2,5 +2,5 @@
 
 apt-get update;
 apt-get install -f;
-wget -qO- "https://deb.nodesource.com/setup_$nodeVer.x" | bash -;
+wget -qO- "https://deb.nodesource.com/setup_8.x" | bash -;
 apt-get install -y nodejs
